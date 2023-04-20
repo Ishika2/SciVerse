cc9ee2081dcf86354536aa84fca584940d9c42fa
lakshay6907 <lg1690104@gmail.com>
2023-04-20 15:58:57 +0530

    signin done

--- app/src/main/java/com/example/sciverse/CustomAuthentication/CustomSigninActivity.kt
+++ app/src/main/java/com/example/sciverse/CustomAuthentication/CustomSigninActivity.kt
@@ -101,25 +101,6 @@ class CustomSigninActivity : AppCompatActivity() {
         binding.textView.setOnClickListener{
             startActivity(Intent(this, PhoneLoginActivity::class.java))
         }
-
-        // on below line we are calling
-        // handler to run a task
-        // for specific time interval
-        Handler().postDelayed({
-            // on below line we are
-            // creating a new intent
-            val i = Intent(
-                this,
-                SplashScreen::class.java
-            )
-            // on below line we are
-            // starting a new activity.
-            startActivity(i)
-
-            // on the below line we are finishing
-            // our current activity.
-            finish()
-        }, 2000)
     }
 
     override fun onStart() {
--- app/src/main/java/com/example/sciverse/SplashScreen.kt
+++ app/src/main/java/com/example/sciverse/SplashScreen.kt
@@ -1,13 +1,19 @@
 package com.example.sciverse
 
 import android.app.Activity
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.os.PersistableBundle
+import android.view.View
 import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
 import android.view.animation.Animation
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sciverse.CustomAuthentication.CustomSigninActivity
+
 //import com.bumptech.glide.Glide;
 
 class SplashScreen : AppCompatActivity() {
@@ -15,17 +21,31 @@ class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash_screen)
+        val content = findViewById<View>(android.R.id.content)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            content.viewTreeObserver.addOnDrawListener { false }
+        }
+        callNextScreen()
+    }
+    private fun callNextScreen(){
+        // on below line we are calling
+        // handler to run a task
+        // for specific time interval
+        Handler().postDelayed({
+            // on below line we are
+            // creating a new intent
+            val i = Intent(
+                this,
+                CustomSigninActivity::class.java
+            )
+            // on below line we are
+            // starting a new activity.
+            startActivity(i)
 
-//        iconAnimation = findViewById(R.id.gif_image)
-//        Glide.with(this).load(R.drawable.android).into(imageView)
-//        val iconImage = findViewById<ImageView>(R.id.icon_image).apply {
-//            setBackgroundResource(R.drawable.icon_animation)
-//            iconAnimation = background as AnimationDrawable
-//        }
-//
-//        iconImage.setOnClickListener({iconAnimation.start()})
-
+            // on the below line we are finishing
+            // our current activity.
+            finish()
+        }, 1000)
     }
 }
